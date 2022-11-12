84b3fd070d3dd78b1bca638fb82131654504164f
yashfalke77 <yashfalke77@gmail.com>
2022-11-12 15:29:42 +0530

    Initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'work', component: WorkComponent
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LogoComponent } from './shared/logo/logo.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { WorkComponent } from './work/work.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogoComponent,
+    NotFoundComponent,
+    WorkComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
